047ae91117caba36851e165e6b792cd779b6cce2
N-bred <thehardyboiz1@gmail.com>
2019-07-07 13:43:36 -0500

    Initial commit, finished app

---
+++ precache-manifest.ad49c0ea6d9e2f44bb1ea59f662df350.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71c0bc7c3044719b9fdf745678a3c36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "223323422219a9d49eb3",
+    "url": "/static/css/main.c4715a14.chunk.css"
+  },
+  {
+    "revision": "ef5600fa136601a5c9f9",
+    "url": "/static/js/2.95b367a5.chunk.js"
+  },
+  {
+    "revision": "223323422219a9d49eb3",
+    "url": "/static/js/main.a0e5cdb5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99a107299db97ffe8a1491d5427ba596",
+    "url": "/static/media/face.99a10729.jpg"
+  },
+  {
+    "revision": "43f803d7f5b9706cade56fa3733a8e39",
+    "url": "/static/media/sell.43f803d7.jpg"
+  }
+]);
